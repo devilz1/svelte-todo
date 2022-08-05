e736aa18eb0fa27a5931e9558334e2995de44f00
Nikolay Matyushkin <devils666_92@mail.ru>
2022-08-05 15:45:28 +0300

    fix: changed text

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import App from './App.svelte';
 
-const app = new App({
+const AppTodo = new App({
 	target: document.body,
 	props: {
 		name: 'world'
 	}
 });
 
-export default app;
+export default AppTodo;
